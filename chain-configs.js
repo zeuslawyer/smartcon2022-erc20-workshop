51ec45068241f9015332874d7cf35211d1c6cd17
Zubin Pratap <zubin.pratap@gmail.com>
2022-09-05 17:30:11 +1000

    Fix incorrect config for Goerli in hh config.  Remove Rinkeby chain data

--- chain-configs.js
+++ chain-configs.js
@@ -9,10 +9,6 @@ const chainConfigs = {
         fee: "100000000000000000",
         // Price Feed Address is obtained dynamically when mocks are deployed.
     },
-    4: {
-        name: "rinkeby",
-        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-    },
     5: {
         name: "goerli",
         ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("@appliedblockchain/chainlink-plugins-fund-link");
-require("dotenv").config();
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("@appliedblockchain/chainlink-plugins-fund-link")
+require("dotenv").config()
 // require("./tasks");
 
 /**
@@ -13,106 +13,98 @@ require("dotenv").config();
  */
 
 const MAINNET_RPC_URL =
-  process.env.MAINNET_RPC_URL ||
-  process.env.ALCHEMY_MAINNET_RPC_URL ||
-  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const GOERLI_RPC_URL =
-  process.env.GOERLI_RPC_URL_HTTP ||
-  "https://eth-goerli.alchemyapi.io/v2/your-api-key";
+    process.env.GOERLI_RPC_URL_HTTP || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 // optional
-const MNEMONIC = process.env.MNEMONIC || "Your mnemonic";
-const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER;
+const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
+const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY =
-  process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
-const POLYGONSCAN_API_KEY =
-  process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key";
-const REPORT_GAS = process.env.REPORT_GAS || false;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      // If you want to do some forking set `enabled` to true
-      forking: {
-        url: MAINNET_RPC_URL,
-        blockNumber: FORKING_BLOCK_NUMBER,
-        enabled: false,
-      },
-      chainId: 31337,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            // If you want to do some forking set `enabled` to true
+            forking: {
+                url: MAINNET_RPC_URL,
+                blockNumber: FORKING_BLOCK_NUMBER,
+                enabled: false,
+            },
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 5,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 1,
+        },
     },
-    localhost: {
-      chainId: 31337,
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+            kovan: ETHERSCAN_API_KEY,
+            polygon: POLYGONSCAN_API_KEY,
+        },
     },
-    goerli: {
-      url: GOERLI_RPC_URL,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      //   accounts: {
-      //     mnemonic: MNEMONIC,
-      //   },
-      saveDeployments: true,
-      chainId: 4,
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
-    mainnet: {
-      url: MAINNET_RPC_URL,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      //   accounts: {
-      //     mnemonic: MNEMONIC,
-      //   },
-      saveDeployments: true,
-      chainId: 1,
+    contractSizer: {
+        runOnCompile: false,
+        only: ["APIConsumer", "KeepersCounter", "PriceConsumerV3", "RandomNumberConsumerV2"],
     },
-  },
-  etherscan: {
-    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
-      rinkeby: ETHERSCAN_API_KEY,
-      kovan: ETHERSCAN_API_KEY,
-      polygon: POLYGONSCAN_API_KEY,
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        feeCollector: {
+            default: 1,
+        },
     },
-  },
-  gasReporter: {
-    enabled: REPORT_GAS,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-  },
-  contractSizer: {
-    runOnCompile: false,
-    only: [
-      "APIConsumer",
-      "KeepersCounter",
-      "PriceConsumerV3",
-      "RandomNumberConsumerV2",
-    ],
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.0",
+            },
+            {
+                version: "0.6.6",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
     },
-    feeCollector: {
-      default: 1,
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
     },
-  },
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.0",
-      },
-      {
-        version: "0.6.6",
-      },
-      {
-        version: "0.4.24",
-      },
-    ],
-  },
-  mocha: {
-    timeout: 200000, // 200 seconds max for running tests
-  },
-};
+}
