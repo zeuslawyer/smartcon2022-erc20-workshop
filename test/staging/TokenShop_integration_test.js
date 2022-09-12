87c1f7b841b593450ae4d043de7227b7c2df1671
Zubin Pratap <zubin.pratap@gmail.com>
2022-09-12 22:35:32 +1000

    Integration tests working. No listener test in integration test.
    *Tests balances for buyer and for TokenShop.
    *Includes calculations of starting balances etc because
    hardhat-deploy reuses contracts, so testnet state needs
    to be accounted for.

--- test/staging/TokenShop_integration_test.js
+++ test/staging/TokenShop_integration_test.js
@@ -1,10 +1,3 @@
-/* INTEGRATION TESTS
-              1. test user 1 who is not deployer can NOT mint from the Shop
-              3. transfer ownership to TokenMinter and then user 1 can mint via TokenShop pay()
-              4. test that on minting an event is emitted.
-              5. Event emit testing: https://hardhat.org/hardhat-chai-matchers/docs/overview#events
-              
-*/
 const { deployments, ethers, network, getNamedAccounts } = require("hardhat")
 const { developmentChains } = require("../../chain-configs")
 const { expect } = require("chai")
@@ -12,7 +5,7 @@ const { expect } = require("chai")
 developmentChains.includes(network.name)
     ? describe.skip
     : describe("TokenShop Integration Tests", async () => {
-          let token, tokenMinter, tokenShop, priceFeed, deployer
+          let token, tokenMinter, tokenShop
 
           before(async () => {
               token = await ethers.getContract("SmartConToken")
@@ -20,55 +13,45 @@ developmentChains.includes(network.name)
               tokenShop = await ethers.getContract("TokenShop")
           })
 
-          describe("Only Owner Can Mint", async () => {
-              it("reverts when TokenMinter is not Owner", async () => {
-                  const sendValue = ethers.utils.parseEther("0.0001")
-                  const [deployer] = await ethers.getSigners()
-
-                  await expect(
-                      tokenShop.connect(deployer).pay({ value: sendValue })
-                  ).to.be.revertedWith("Ownable: caller is not the owner")
-              })
-
-              it.only("Mints tokens when Token Minter is Owner", async () => {
+          describe("Non Owner cannot mint", async () => {
+              it("Mints tokens when Token Minter is Owner", async () => {
                   const [deployer] = await ethers.getSigners()
 
                   let o = await token.owner()
-                  console.log("  Token's current owner is ", o)
-
-                  //   const ownershipTransferTx = await token
-                  //       .connect(deployer)
-                  //       .transferOwnership(tokenMinter.address)
-                  //   ownershipTransferTx.wait(1)
+                  if (o === deployer.address) {
+                      console.log(
+                          `Transferring ownership from ${deployer.address} to ${tokenMinter.address} `
+                      )
+                      const ownershipTransferTx = await token
+                          .connect(deployer)
+                          .transferOwnership(tokenMinter.address)
+                      ownershipTransferTx.wait(1)
+                  }
 
                   const sendValue = ethers.utils.parseEther("0.000001")
 
                   // We use the deploying account as the buyer for testing on testnets as other addresses are not configured
                   // and may not have enough ETH to buy SmartCon Tokens.
                   const deployerStartingBalance = await token.balanceOf(deployer.address)
-
-                  let payTx = await tokenShop.connect(deployer).pay({ value: sendValue })
-                  await payTx.wait(1)
-
-                  // Check tokenShop balance is increased.
-                  const tokenContractBal = await ethers.provider.getBalance(tokenShop.address)
-                  //   expect(tokenContractBal.toString()).to.equal(
-                  //       Number(sendValue.toString())
-                  //   )
-
-                  const deployerEndingBalance = await token.balanceOf(deployer.address)
-                  const tokensReceived = deployerEndingBalance.sub(deployerStartingBalance)
-                  const numTokensMinted = await tokenShop.calculateTokens(sendValue)
-
-                  console.log(
-                      "Starting, ending and received : ",
-                      deployerStartingBalance.toString(),
-                      deployerEndingBalance.toString(),
-                      tokensReceived.toString(),
-                      numTokensMinted.toString()
+                  const tokenShopStartingBalance = await ethers.provider.getBalance(
+                      tokenShop.address
                   )
 
-                  expect(tokensReceived.toString()).to.equal(numTokensMinted.toString())
+                  let payTx = await tokenShop.connect(deployer).pay({ value: sendValue })
+                  await expect(payTx.wait(1)).to.not.be.revertedWith(
+                      "Ownable: caller is not the owner"
+                  )
+
+                  const deployerEndingBalance = await token.balanceOf(deployer.address)
+                  const tokenShopEndingBalance = await ethers.provider.getBalance(tokenShop.address)
+                  const tokenShopEarnings = tokenShopEndingBalance.sub(tokenShopStartingBalance)
+
+                  const numTokensMinted = await tokenShop.calculateTokens(sendValue)
+                  const tokensReceived = deployerEndingBalance.sub(deployerStartingBalance)
+
+                  // Check updated states.
+                  expect(tokensReceived).to.equal(numTokensMinted)
+                  expect(tokenShopEarnings).to.equal(sendValue)
               })
           })
       })
